221548ca8ce77de521ff83db4aaca9d84b5a47a3
Kendall Cruz Soto <kendallsoto51@gmail.com>
2024-12-03 20:38:49 +0000

    Commit 0.0.5: Funciones(), json, etc

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -3,9 +3,8 @@ let countriesList = [];
 
 async function LoadData() {
     try {
-        const response = await fetch('https://restcountries.com/v3.1/all?fields=name,flags');
+        const response = await fetch('https://restcountries.com/v3.1/all');
         const data = await response.json();  
-        console.log(data)
         countriesList = data;
         return data;
     } catch (error) {
@@ -17,24 +16,24 @@ async function LoadData() {
 async function processData() {
     let data = await LoadData();  
 
-    if (data) {
-        displayCountries(data); 
+    if (data.length > 0) {  
+        console.log("Si hay datos")
+        displayCountries(data);
     } else {
-        console.log("No se pudieron obtener los datos.");
+        console.log("No data available or failed to load.");
     }
 }
 
 function displayCountries(countries) {
-    
+
     let container = document.getElementById('api-country-list');
     container.innerHTML = '';
-    
+    console.log("Si se cargaron los datos")
     countries.forEach(country => {
         let section = document.createElement('section');
-
         section.classList.add('country-card');
 
-        section.innerHTML=`
+        section.innerHTML = `
             <div class="country-flag">
                 <img src="${country.flags.svg}" alt="" id="flag">
             </div>
@@ -42,9 +41,8 @@ function displayCountries(countries) {
                 <h3 class="country-name">${country.name.common}</h3>
                 <p class="official-name">${country.name.official}</p>     
             </div>
-        `
-        container.appendChild(section)
-        console.log(section)
+        `;
+        container.appendChild(section);
     });
 }
 
@@ -55,30 +53,64 @@ function filterCountries(query) {
 }
 
 function handleSearch(event) {
+    console.log("Se esta intentando buscar")
     const query = event.target.value;  
-    const filteredUsers = filterCountries(query);  
-    displayCountries(filteredUsers);  
+    const filteredCountries = filterCountries(query);  
+    displayCountries(filteredCountries);  
 }
 
 function filterCountriesByRegion(region) {
     return countriesList.filter(country => 
         country.region.toLowerCase() === region.toLowerCase()
+        
     );
 }
 
+function SelectChange(event){
+    const value = event.target.value;
+    const filteredCountriesRegion = filterCountriesByRegion(value);
+    displayCountries(filteredCountriesRegion);
+}
 
+(async function () {
+    const data = await processData();
+    // displayCountries(data);
 
+    try {
+        const urlParams = new URLSearchParams(window.location.search);
 
-document.getElementById('txtCountry').addEventListener('input', handleSearch);
-LoadData();
-displayCountries(processData())
+        const formValue = urlParams.get('form');
 
+        const response = await fetch('/docs/scripts/html.json');
+        
+        if (!response.ok) {
+            throw new Error("Error al cargar el archivo JSON");
+        }
 
-console.log("aasdasd",countriesList)
+        const data = await response.json();
 
+        if (data.form && data.form[formValue]) {
+            
+            let form = document.querySelector('.form-input');
+            form.innerHTML = data.form[formValue].html;  
+        } else {
+            throw new Error('La página solicitada no existe en el JSON');
+        }
 
+    } catch (error) {
+        
+    }
 
+    let select = document.getElementById('regions')
+    if(select){
+        select.addEventListener('change', SelectChange)
+    }
 
+    let txtBox = document.getElementById('txtCountry')
+    if(txtBox){
+        txtBox.addEventListener('input', handleSearch);
+    }
+}())
 
 
 
